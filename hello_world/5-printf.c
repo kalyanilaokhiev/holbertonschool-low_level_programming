8d0c7c99ef0481f5e31129b67c8d49a1be663cc6
Kalyani <14894@holbertonstudents.com>
2026-07-07 11:26:52 +0000

    task 5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - starting point
+ *
+ * Description: using printf to print string
+ * Return: zero
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
