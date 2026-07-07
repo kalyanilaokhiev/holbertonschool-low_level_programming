bc473e94f84dc0b5ae767e0658bed39e375f5634
Kalyani <14894@holbertonstudents.com>
2026-07-07 11:10:43 +0000

    task 4

---
+++ hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: program that prints a line of strings
+ * Return: zero
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
