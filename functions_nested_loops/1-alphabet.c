bb63e11f6bee9e01a1d1cafd61e8f6bb67034c86
Kalyani <14894@holbertonstudents.com>
2026-07-09 13:07:52 +0000

    task 1 and main updated

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - starting point
+ *
+ * Return: zero
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar(letter);
+	}
+	_putchar('\n');
+}
