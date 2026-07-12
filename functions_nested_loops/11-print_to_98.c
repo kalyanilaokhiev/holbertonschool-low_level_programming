7ca7bca17957aa3bd3e0418cca5d399d89399c29
Kalyani <14894@holbertonstudents.com>
2026-07-12 12:06:05 +0000

    task 11 and main

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * print_to_98 - starting point
+ * @n: starting number
+ *
+ * Description: counts up or down to 98
+ * Return: the numbers to get to 98
+ */
+
+void print_to_98(int n)
+{
+	while (n != 98)
+	{
+		int number = n;
+
+		if (number < 0)
+		{
+			_putchar('-');
+			number = -number;
+		}
+
+		if (num >= 100)
+		{
+			_putchar((number / 100) + '0');
+			_putcahr(((number / 10) % 10) + '0');
+		}
+		else if (number >= 10)
+		{
+			_putchar((number / 10) + '0');
+		}
+		_putchar((number % 10) + '0');
+
+		_putchar(',');
+		_putchar(' ');
+
+		if (n < 98)
+			n++;
+		else
+			n--;
+	}
