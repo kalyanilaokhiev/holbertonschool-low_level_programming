84649db1d5fa1daf60f7bbf90ccd308cb6046911
Kalyani <14894@holbertonstudents.com>
2026-07-12 12:42:27 +0000

    task 1 and main

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/** 
+ * _isdigit - starting point
+ * @c: input character
+ *
+ * Return: 1 if c digit, 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
