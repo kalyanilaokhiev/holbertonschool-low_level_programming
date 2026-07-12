c3a05d613d30be8527bec94c0bcf5cd36f9bebb5
Kalyani <14894@holbertonstudents.com>
2026-07-12 02:16:20 +0000

    task 6 main

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - starting point
+ * @n: input number
+ *
+ * Description: computes absolute value
+ * Return: absolute value
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
