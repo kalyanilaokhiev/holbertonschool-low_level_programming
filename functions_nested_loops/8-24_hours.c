2300419791768d24cb88033c3dcc1ef0a8c68ef3
Kalyani <14894@holbertonstudents.com>
2026-07-12 10:56:44 +0000

    adding minutes

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -14,12 +14,14 @@ void jack_bauer(void)
 
 		for (hour = 0; hour < 24; hour++)
 		{
-			_putchar((hour / 10) + '0');
-			_putchar((hour % 10) + '0');
-			_putchar(':');
-			_putchar((minute / 10) + '0');
-			_putchar((minute % 10) + '0');
-			_putchar('\n');
+			for (col = 0; col < 60; minute++)
+			{
+				_putchar((hour / 10) + '0');
+				_putchar((hour % 10) + '0');
+				_putchar(':');
+				_putchar((minute / 10) + '0');
+				_putchar((minute % 10) + '0');
+				_putchar('\n');
+			}
 		}
 	}
-}
