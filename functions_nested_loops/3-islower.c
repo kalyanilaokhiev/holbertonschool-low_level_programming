03b9688343e0cf6380003f78b62d6b8ed4fff179
Kalyani <14894@holbertonstudents.com>
2026-07-12 01:44:06 +0000

    task 3 and main

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower - starting point
+ *
+ * Description: checks for lowercase
+ * Return: 1 is c lowercase, 0 otherwise
+ */
+
+int _islower(int c)
+{
+	if (c <= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
