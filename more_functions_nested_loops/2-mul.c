0499b5eca3add5ba24ab7777d9d9f55cbc17d683
Kalyani <14894@holbertonstudents.com>
2026-07-12 12:48:51 +0000

    task 2 and main

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - starting point
+ * @a: first number
+ * @b: second number
+ *
+ * Return: multiplied solution
+ */
+
+int mul(int a, int b)
+{
+	int answer;
+
+	answer = (a * b);
+	return (answer);
+}
