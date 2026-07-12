779ebbca079a449cce3a449e8dbf3bc306df1224
Kalyani <14894@holbertonstudents.com>
2026-07-12 11:44:40 +0000

    task 10 main

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - starting point
+ * @first: first number
+ * @second: second number
+ *
+ * Description: adds 2 integers together
+ * Return: results
+ */
+
+int add(int first, int second)
+{
+	int result;
+
+	result = (first + second);
+	return (result);
+}
