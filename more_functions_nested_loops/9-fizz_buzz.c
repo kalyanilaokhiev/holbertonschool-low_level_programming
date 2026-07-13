7d2e45e82359b0301771fdc4a171b984eefb3f4a
Kalyani <14894@holbertonstudents.com>
2026-07-13 05:07:07 +0000

    task 9 main

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * fizz_buzz - starting point
+ *
+ * Description: prints 1 - 100 with buzz words
+ * Return: 0
+ */
+
+void fizz_buzz(void)
+{
+	int n;
+
+	for (n = 0; n <= 100; n++)
+	{
+		if (n % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (n % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (n % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			print("%d", n);
+		}
+	}
+	printf("\n");
+	return (0);
+}
