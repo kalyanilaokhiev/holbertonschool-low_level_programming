2987eb6798a4e380588a9d83deaf7b3325ab4e2c
Kalyani <14894@holbertonstudents.com>
2026-07-13 04:41:51 +0000

    task 7 main

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_diagonal - starting point
+ * @n: number of \
+ *
+ * Description: draws a diagonal line
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	if (n <= 0)
+	{
+		_putchar('\n')
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
