107c6093e387997a5f6ee475335bdb0856739dcd
Kalyani <14894@holbertonstudents.com>
2026-07-14 04:43:52 +0000

    task 2

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -29,7 +29,24 @@ int main(void)
 		{
 			printf("Invalid choice\n");
 		}
-		if (n == 0)
+
+		if (n == 1)
+		{
+			int A;
+			int B;
+			int result;
+
+			printf("A: ");
+			scanf("%d", &A);
+
+			printf("B: ");
+			scanf("%d", &B);
+
+			result = (A + B);
+
+			printf("Result: %d\n", result);
+		}
+		else if (n == 0)
 		{
 			printf("Bye!\n");
 		}
