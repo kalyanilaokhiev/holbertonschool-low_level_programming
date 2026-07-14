afbbd47dc218764ca5d82fb091dabfabb8a611a1
Kalyani <14894@holbertonstudents.com>
2026-07-14 12:45:35 +0000

    task 1

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps values of a and b
+ * @a: first int
+ * @b: second int
+ *
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int swap;
+	swap = *a;
+	*a = *b;
+	*b = swap;
+}
