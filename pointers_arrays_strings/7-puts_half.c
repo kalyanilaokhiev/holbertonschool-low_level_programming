cd9962d04c92d2b4f7b5dfc27e850fa23e1d375d
Kalyani <14894@holbertonstudents.com>
2026-07-15 10:56:28 +0000

    task 7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,36 @@
+#inlcude "main.h"
+
+/**
+ * puts_half(char *str);
+ * @str: string
+ *
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int length = 0;
+
+	while (s[length] != '/0')
+	{
+		length++;
+	}
+
+	if (length % 2 == 0)
+	{
+		split = length / 2;
+	}
+	else
+	{
+		/* if length is odd add 1 to make even */
+		split = (length + 1) / 2;
+	}
+
+	/* print string */
+	while (str[split] != '\0')
+	{
+		_putchar(str[split]);
+		split++;
+	}
+	_putchar('\n');
+}
