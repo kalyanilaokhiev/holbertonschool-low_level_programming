bc5d8dce1d7d72e45e47bbaaef8ea7da1696bbb0
Kalyani <14894@holbertonstudents.com>
2026-07-15 11:33:56 +0000

    task 9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcpy - function that copies string pointed to by src
+ * @dest: destination
+ * @src: source string pointer
+ *
+ * Return: pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (scr[i] != '\0')
+	{
+		dest[i] = scr[i]
+			i++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
