3cbccea34cb60b346eab106938873d6dfa01b163
Kalyani <14894@holbertonstudents.com>
2026-07-16 03:49:08 +0000

    betty compliant

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -2,15 +2,88 @@
 
 /**
  * main - starting point
+ * add: adding funtion
+ * subtract: subtract function
+ * multiply - multiply function
+ * divide - divide function
  *
  * Description: printing welcome line, menu and
  * quitting with option 0
  * Return: void
  */
 
+void add(void)
+{
+	int A;
+	int B;
+	int result;
+
+	printf("A: ");
+	scanf("%d", &A);
+
+	printf("B: ");
+	scanf("%d", &B);
+
+	result = (A + B);
+
+	printf("Result: %d\n", result);
+}
+
+void subtract(void)
+{
+	int A;
+	int B;
+	int result;
+
+	printf("A: ");
+	scanf("%d", &A);
+	printf("B: ");
+	scanf("%d", &B);
+
+	result = (A - B);
+	printf("Result: %d\n", result);
+}
+
+void multiply(void)
+{
+	int A;
+	int B;
+	int result;
+
+	printf("A: ");
+	scanf("%d", &A);
+	printf("B: ");
+	scanf("%d", &B);
+
+	result = (A * B);
+	printf("Result: %d\n", result);
+}
+
+void divide(void)
+{
+	int A;
+	int B;
+	int result;
+
+	printf("A: ");
+	scanf("%d", &A);
+	printf("B: ");
+	scanf("%d", &B);
+
+	if (B == 0)
+	{
+		printf("Error: division by zero\n");
+	}
+
+	else
+	{
+		result = (A / B);
+		printf("Result: %d\n", result);
+	}
+}
+
 int main(void)
 {
-	/* need to make it an invalid choice so it doesnt automatically quit*/
 	int n = -1;
 
 	printf("Simple Calculator\n");
@@ -19,90 +92,35 @@ int main(void)
 	printf("3) Multiply\n");
 	printf("4) Divide\n");
 	printf("0) Quit\n");
-	
+
 	while (n != 0)
 	{
 		printf("Choice: ");
 		scanf("%d", &n);
 
-		if (n != 0 && n != 1 && n != 2 && n != 3 && n != 4)
-		{
-			printf("Invalid choice\n");
-		}
-
 		if (n == 1)
 		{
-			int A;
-			int B;
-			int result;
-
-			printf("A: ");
-			scanf("%d", &A);
-
-			printf("B: ");
-			scanf("%d", &B);
-
-			result = (A + B);
-
-			printf("Result: %d\n", result);
+			add();
 		}
 		else if (n == 2)
 		{
-			int A;
-			int B;
-			int result;
-
-			printf("A: ");
-			scanf("%d", &A);
-
-			printf("B: ");
-			scanf("%d", &B);
-
-			result = (A - B);
-
-			printf("Result: %d\n", result);
+			subtract();
 		}
 		else if (n == 3)
 		{
-			int A;
-			int B;
-			int result;
-
-			printf("A: ");
-			scanf("%d", &A);
-
-			printf("B: ");
-			scanf("%d", &B);
-
-			result = (A * B);
-
-			printf("Result: %d\n", result);
+			multiply();
 		}
 		else if (n == 4)
 		{
-			int A;
-			int B;
-			int result;
-
-			printf("A: ");
-			scanf("%d", &A);
-
-			printf("B: ");
-			scanf("%d", &B);
-
-			if (B == 0)
-			{
-				printf("Error: division by zero\n");
-			}
-			else
-			{
-				result = (A / B);
-				printf("Result: %d\n", result);
-			}
+			divide();
 		}
 		else if (n == 0)
 		{
 			printf("Bye!\n");
+		}
+		else
+		{
+			printf("Invalid choice\n");
 		}
 	}
 	return (0);
