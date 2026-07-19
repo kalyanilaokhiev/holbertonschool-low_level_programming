4e0112ab9b1b75d2da30ba1735f2f3ce19e9b78d
Kalyani <14894@holbertonstudents.com>
2026-07-19 17:53:32 +1000

    task 2

---
+++ benchmarking/instrumentation_lab.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <time.h>
+
+#define DATASET_SIZE 50000
+#define SEED_VALUE 42u
+
+static int dataset[DATASET_SIZE];
+
+static unsigned int next_value(unsigned int *state)
+{
+    *state = (*state * 1103515245u) + 12345u;
+    return *state;
+}
+
+static void build_dataset(void)
+{
+    unsigned int state;
+    int i;
+
+    state = SEED_VALUE;
+
+    for (i = 0; i < DATASET_SIZE; i++)
+        dataset[i] = (int)(next_value(&state) % 100000);
+}
+
+static void process_dataset(void)
+{
+    int i;
+    int v;
+
+    for (i = 0; i < DATASET_SIZE; i++)
+    {
+        v = dataset[i];
+        v = (v * 3) + (v / 7) - (v % 11);
+        if (v < 0)
+            v = -v;
+        dataset[i] = v;
+    }
+}
+
+static unsigned long reduce_checksum(void)
+{
+    unsigned long sum;
+    int i;
+
+    sum = 0;
+    for (i = 0; i < DATASET_SIZE; i++)
+        sum = (sum * 131ul) + (unsigned long)dataset[i];
+
+    return sum;
+}
+
+int main(void)
+{
+    unsigned long checksum;
+    clock_t total_start, total_end;
+    clock_t phase_start, phase_end;
+    double time_total, time_build, time_process, time_reduce;
+
+    total_start = clock();
+
+    phase_start = clock();
+    build_dataset();
+    phase_end = clock();
+    time_build = (double)(phase_end - phase_start) / CLOCKS_PER_SEC;
+
+    phase_start = clock();
+    process_dataset();
+    phase_end = clock();
+    time_process = (double)(phase_end - phase_start) / CLOCKS_PER_SEC;
+
+    phase_start = clock();
+    build_dataset();
+    process_dataset();
+    checksum = reduce_checksum();
+    phase_end = clock();
+    time_reduce = (double)(phase_end - phase_start) / CLOCKS_PER_SEC;
+
+    if (checksum == 0ul)
+        printf("impossible\n");
+
+    total_end = clock();
+    time_total = (double)(total_end - total_start) / CLOCKS_PER_SEC;
+
+    printf("TOTAL seconds: %.6f\n", time_total);
+    printf("BUILD_DATA seconds: %.6f\n", time_build);
+    printf("PROCESS seconds: %.6f\n", time_process);
+    printf("REDUCE seconds: %.6f\n", time_reduce);
+
+    /* Required output (exact format, no extra lines):
+     * TOTAL seconds: <float>
+     * BUILD_DATA seconds: <float>
+     * PROCESS seconds: <float>
+     * REDUCE seconds: <float>
+     */
+
+    return 0;
+}
