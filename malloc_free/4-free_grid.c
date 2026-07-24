1086dfe85b9e70a756c96ba72c009eeaada0e182
Kalyani <14894@holbertonstudents.com>
2026-07-24 10:26:16 +1000

    un indent

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -9,11 +9,11 @@ void free_grid(int **grid, int height)
 	if (grid == NULL || height <= 0)
 		return;
 
-		while (height > 0)
-		{
-			height--;
-			free(grid[height]);
-		}
+	while (height > 0)
+	{
+		height--;
+		free(grid[height]);
+	}
 
-		free(grid);
+	free(grid);
 }
