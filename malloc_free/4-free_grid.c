d4b9086d03b9bdd624154e77c449b32595d315ae
Kalyani <14894@holbertonstudents.com>
2026-07-24 10:41:54 +1000

    assign comments

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -2,6 +2,11 @@
 #include <stdlib.h>
 
 /**
+ * free_grid - function that frees a 2d grid
+ * @grid: the grid filled with numbers
+ * @height: the columns
+ *
+ * Return: nothing
  */
 
 void free_grid(int **grid, int height)
