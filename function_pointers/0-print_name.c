d21c63798fea779432ebfb44f4568ef7a53f5540
Kalyani <14894@holbertonstudents.com>
2026-07-30 12:34:20 +1000

    making it betty compliant

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * print_name - prints a name
+ * @name: name inputted
+ * @f: the functions print_name_uppercase or print_name_as_is
+ *
+ * return: void
+ */
+
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL && f == NULL)
