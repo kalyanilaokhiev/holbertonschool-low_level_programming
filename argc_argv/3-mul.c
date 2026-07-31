d421d8f03983220b76cd594c97b46d24e1267480
Kalyani <14894@holbertonstudents.com>
2026-07-31 13:07:41 +1000

    task 3

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies
+ *
+ * @argc: number of strings in array
+ * @argv: array of strings in argc
+ *
+ * Return: multiplies strings
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int mul = 0;
+
+	if(argc == 3)
+	{
+		/* 1 because you dont want the name of the program*/
+		for(i = 1; i < argc; i++)
+		{
+			int num1 = atoi(argv[1]);
+			int num2 = atoi(argv[2]);
+
+			mul = (num1 * num2);
+		}
+		printf("%d\n", mul);
+	}
+	return (0);
+}
