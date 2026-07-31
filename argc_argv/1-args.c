76eeb454e78acb855d39a36e6ee3a56f703765b2
Kalyani <14894@holbertonstudents.com>
2026-07-31 12:50:12 +1000

    task 1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints number of args
+ *
+ * @argc: number of strings in array
+ * @argv: array of strings in argc
+ *
+ * Return: number of args
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argv)
+	{
+	}
+
+	printf("%d\n", argc);
+
+	return (0);
+}
