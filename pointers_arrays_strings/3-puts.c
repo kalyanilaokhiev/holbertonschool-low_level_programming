f19af5152d14bda2883e5045a38ccb41db21ff83
Kalyani <14894@holbertonstudents.com>
2026-08-03 18:46:04 +1000

    task 3 redo

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,20 +1,28 @@
 #include "main.h"
 
 /**
- * _puts - prints a string
- * @str: pointer to string to print
- *
- * Return: void
+ * _puts - prints string
+ * @str: pointer to the string
  */
 
 void _puts(char *str)
 {
-	int i = 0;
+	int i;
 
+	i = 0;
+
+	/* output each character 1 by one going through entire string */
 	while (str[i] != '\0')
 	{
 		_putchar(str[i]);
 		i++;
 	}
+
+	/* when reach end, print new line */
+	while (str[i] == '\0')
+	{
+		_putchar('\n');
+	}
 	_putchar('\n');
+
 }
