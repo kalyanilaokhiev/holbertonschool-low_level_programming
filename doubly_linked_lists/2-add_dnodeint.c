b0f470f0bea6188ea148ae65ac70760c080e92a4
Kalyani <14894@holbertonstudents.com>
2026-08-10 12:49:10 +1000

    checking if list is empty

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -2,6 +2,14 @@
 #include <string.h>
 #include <stdlib.h>
 
+/**
+ * add_dnodeint - adding node to the front
+ * @head: pointer to first node struct
+ * @n: integer input
+ *
+ * Return: addr of new element or NULL if failed
+ */
+
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node;
@@ -15,6 +23,10 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new_node->n = n;
 	new_node->next = *head;
 	new_node->prev = NULL;
+
+	/* if list is not empty */
+	if (*head != NULL)
+		(*head)->prev = new_node;
 
 	*head = new_node;
 
