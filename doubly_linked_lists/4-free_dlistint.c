795fb6c5e64314113fb9eb13375198461ca6fcb3
Kalyani <14894@holbertonstudents.com>
2026-08-10 14:36:27 +1000

    task 4

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * free_dlistint - free list
+ * @h: pointer to struct
+ */
+void free_dlistint(dlistint_t *head)
+{
+	/* create temporary node pointer */
+	dlistint_t *temp;
+
+	while (head != NULL)
+	{
+		/* copy next addr into temp*/
+		temp = head->next;
+		/* free memory addr */
+		free(head);
+		/* update freed head to temp */
+		head = temp;
+	}
+}
