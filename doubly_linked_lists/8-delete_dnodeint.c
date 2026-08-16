2a1feb197bbf8ad7d5783728c64bf54cdcfbfd6c
Kalyani <14894@holbertonstudents.com>
2026-08-16 14:19:52 +1000

    .

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -47,7 +47,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 
 	/* if not last node, update next node prev pointer */
 	if (current_node->next != NULL)
-		current_node->next->prev = current_prev->prev;
+		current_node->next->prev = current_node->prev;
 
 	free(current_node);
 	return (1);
