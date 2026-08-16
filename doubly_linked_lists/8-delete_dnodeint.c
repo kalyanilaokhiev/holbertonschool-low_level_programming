979c520aec4ccf522645692a591855d23149d5ea
Kalyani <14894@holbertonstudents.com>
2026-08-16 14:19:07 +1000

    wrong name

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -47,7 +47,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 
 	/* if not last node, update next node prev pointer */
 	if (current_node->next != NULL)
-		current_node->next->prev = current->prev;
+		current_node->next->prev = current_prev->prev;
 
 	free(current_node);
 	return (1);
