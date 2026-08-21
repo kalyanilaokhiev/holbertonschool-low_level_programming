4b73c34b75924c7ef1a3fc9c0f300f888c92026d
Kalyani <14894@holbertonstudents.com>
2026-08-21 21:22:54 +1000

    trying to decrease code

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -16,25 +16,11 @@ void dup_data(int file_from, int file_to, char *argv[])
 	char buffer[1024];
 	ssize_t bytes_read, writing;
 
+	/* must read 1,024 bytes at a time */
 	while ((bytes_read = read(file_from, buffer, 1024)) > 0)
 	{
+		/* write exactly number of bytes read */
 		writing = write(file_to, buffer, bytes_read);
-
-		if (writing == -1 || writing != bytes_read)
-		{
-			close(file_from);
-			close(file_to);
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			exit(99);
-		}
-	}
-
-	if (bytes_read == -1)
-	{
-		close(file_from);
-		close(file_to);
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
 	}
 }
 
@@ -50,6 +36,7 @@ int main(int argc, char *argv[])
 {
 	int file_from, file_to;
 
+	/* num of arg not correct */
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
@@ -57,13 +44,16 @@ int main(int argc, char *argv[])
 	}
 	file_from = open(argv[1], O_RDONLY); /* open first file */
 
+	/* if file_from does not exist, or if you can not read it,  */
 	if (file_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
+	/* if file_to already exists, truncate it */
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
+	/* if you can not create or if write to file_to */
 	if (file_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
@@ -72,6 +62,7 @@ int main(int argc, char *argv[])
 
 	dup_data(file_from, file_to, argv);
 
+	/* if you can not close a file descriptor */
 	if (close(file_from) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
