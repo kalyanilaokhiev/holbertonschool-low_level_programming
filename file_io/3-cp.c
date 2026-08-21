9b1ad3269d08a4842a2bda944764e1b63a1b136c
Kalyani <14894@holbertonstudents.com>
2026-08-21 21:05:04 +1000

    adding error msg

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -70,8 +70,16 @@ int main(int argc, char *argv[])
 		exit(99);
 	}
 
-	close_file(file_from);
-	close_file(file_to);
+	if (close(file_from) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
 
+	if (close(file_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
+		exit(100);
+	}
 	return (0);
 }
