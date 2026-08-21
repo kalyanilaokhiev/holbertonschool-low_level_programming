0f13e0e10558b2b22e5dd22d1af316502e0c3239
Kalyani <14894@holbertonstudents.com>
2026-08-21 13:35:05 +1000

    task 1

---
+++ file_io/1-create_file.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+
+/**
+ * create_file - creates a file
+ * @filename: name of the file to create
+ * @text_content: a NULL terminated string
+ *
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t writing = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	/* if text_content is NULL create an empty file */
+	if (text_content == NULL)
+		text_content = "";
+
+	/** 600 = rw------- */
+	fd = open(filename, O_CREAT | O_WRONLY, 600);
+
+	if (fd == -1)
+		return (-1);
+
+	writing = write(fd, text_content, strlen(text_content));
+	if (writing == -1)
+		return (-1);
+
+	close(fd);
+	return (1);
+}
