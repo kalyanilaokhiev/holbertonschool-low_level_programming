280237b997114a5ce528b037135f89058f31709b
Kalyani <14894@holbertonstudents.com>
2026-08-21 20:44:59 +1000

    closing file from

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -13,51 +13,44 @@ int main(int argc, char *argv[])
 	ssize_t bytes_read, writing;
 	char buffer[1024];
 
-	/* name + 2 files = 3 */
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
-	/* open first file */
-	file_from = open(argv[1], O_RDONLY);
+	file_from = open(argv[1], O_RDONLY); /* open first file */
 	
 	if (file_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
-	/* open and create second file */
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 	if (file_to == -1)
 	{
+		close(file_from);
 		dprintf(STDERR_FILENO, "Can't write to %s\n", argv[2]);
 		exit(99);
 	}
-
 	while ((bytes_read = read(file_from, buffer, 1024)) > 0)
 	{
 		writing = write(file_to, buffer, bytes_read);
 
 		if (writing == -1 || writing != bytes_read)
 		{
-			close(file_from); 
+			close(file_from);
 			close(file_to);
 			dprintf(STDERR_FILENO, "Error: Can't close fd %s\n", argv[2]);
 			exit(100);
 		}
 	}
-
-	if (bytes_read == -1) 
-	{ 
-		close(file_from); 
+	if (bytes_read == -1)
+	{
+		close(file_from);
 		close(file_to);
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]); 
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
 	return (0);
 }
